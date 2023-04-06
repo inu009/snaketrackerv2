ed8c689d9ce9527ba0eec9d025f9deeb3ba2a49d
Parnell Steide <parnellsteide@Parnells-Air.(none)>
2023-04-06 13:38:50 -0400

    Initial Commit

---
+++ src/main/java/com/psteide/snaketrackerapiv2/Snaketrackerapiv2Application.java
@@ -0,0 +1,13 @@
+package com.psteide.snaketrackerapiv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Snaketrackerapiv2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Snaketrackerapiv2Application.class, args);
+	}
+
+}
