842017561acf1e6d6d6340a6bd9a95121750c172
Parnell Steide <parnellsteide@Parnells-Air.(none)>
2023-04-07 11:29:52 -0400

    Add cross origin annotation to controllers

--- src/main/java/com/psteide/snaketrackerapiv2/controller/SnakeController.java
+++ src/main/java/com/psteide/snaketrackerapiv2/controller/SnakeController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/snakes")
 public class SnakeController {
